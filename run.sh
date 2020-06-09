9db8a313b273cdfaff04cac657cfedd0366c65d1
Artur Tarassow <atecon@posteo.de>
2020-06-09 09:51:18 +0200

    drop git stuff again

--- run.sh
+++ run.sh
@@ -6,8 +6,6 @@ DIR=$(dirname $(realpath "$0")) 	# locate folder where this sh-script is located
 cd $DIR
 echo "Switched to ${DIR}"
 
-git pull
-
 # Drop old figures with forecasts
 rm -rf ./figures/*.png
 mkdir -p figures
@@ -19,14 +17,9 @@ if [ $? -eq 0 ]
 then
   echo "Finished analysis"
   rm string_table.txt
+  exit 0
 else
   echo "Failure: Some error occured."
   rm string_table.txt
   exit 1
 fi
-
-git add .
-git commit -m "update to $(date '+%Y-%m-%d')"
-git push
-
-exit 0
